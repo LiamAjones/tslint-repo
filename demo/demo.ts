4f0196d4b8d089414c173704bdd7991cb7834b5c
LiamAjones <Liam.andrew.jones@outlook.com>
2020-09-04 08:48:32 +0100

    add alot of errors

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,5 +1,121 @@
 const istesting: boolean = true;
 
 if (istesting == true) {
-    console.log('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+}
+
+
+const istesting: boolean = true;
+
+if (istesting == true) {
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+}
+
+
+const istesting: boolean = true;
+
+if (istesting == true) {
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+}
+
+
+const istesting: boolean = true;
+
+if (istesting == true) {
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+}
+
+
+const istesting: boolean = true;
+
+if (istesting == true) {
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
+    console.debug('demo1');
 }
